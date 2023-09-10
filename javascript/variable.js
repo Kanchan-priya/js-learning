d3a4a1742bacd42b36365cbeb4c3da8cc888cf3c
Kanchan-priya <139807735+Kanchan-priya@users.noreply.github.com>
2023-09-10 09:18:34 +0000

    container config done

---
+++ javascript/variable.js
@@ -0,0 +1,13 @@
+
+const accountId=1234
+var accountEmail="kanchan@gmail.com";
+let accountPassword=12345;
+accountCity="jaypur";
+let accountState;
+
+
+accountEmail="Kanchanpriya@gmail.com";
+accountPassword=123;
+accountCity="karnatka"
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
